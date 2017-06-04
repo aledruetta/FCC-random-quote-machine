ee5bedf07cc182c13f9cea760f86feaa79dda5f2
Alejandro Druetta <aledruetta@gmail.com>
2017-06-04 20:01:18 -0300

    ajax request api

--- src/app.js
+++ src/app.js
@@ -1,8 +1,33 @@
 $(function() {
+  'use strict';
 
   function AppViewModel() {
-    this.quoteText = ko.observable('If we have a positive mental attitude, then even when surrounded by hostility, we shall not lack inner peace.');
-    this.quoteAuthor = ko.observable('Dalai Lama');
+    var self = this;
+
+    this.quoteText = ko.observable('');
+    this.quoteAuthor = ko.observable('');
+
+    this.twitterPost = function() {};
+
+    this.facebookPost = function() {};
+
+    this.newQuote = function() {
+      var url = 'https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&callback=?';
+
+      // $.getJSON(url)
+      $.ajax({
+        url: 'https://api.forismatic.com/api/1.0/?',
+        dataType: 'jsonp',
+        data: 'method=getQuote&format=jsonp&lang=en&jsonp=?',
+      })
+      .done(function(data) {
+        self.quoteAuthor(data.quoteAuthor);
+        self.quoteText(data.quoteText);
+      })
+      .fail();
+    };
+
+    this.newQuote();
   }
 
   ko.applyBindings(new AppViewModel());
